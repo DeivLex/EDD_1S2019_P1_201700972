a0d80eeba0e0eaabf7560f72a43f1cb8903993ab
DeivLex <peloliso.98@gmail.com>
2019-08-10 22:45:26 -0600

    Lista Circular Doble completada

---
+++ ListaCircularDoble.py
@@ -0,0 +1,72 @@
+class Nodo:
+    def __init__(self,dato):
+        self.dato = dato
+        self.siguiente = None
+        self.anterior = None
+
+class ListaCircularDoble:
+
+    def __init__(self):
+        self.primero = None
+        self.ultimo = None
+
+    def vacia(self):
+        if self.primero == None:
+            return True
+        else:
+            return False
+    
+    def agregar_inicio(self,dato):
+        if self.vacia():
+            self.primero = self.ultimo = Nodo(dato)
+        else:
+            aux = Nodo(dato)
+            aux.siguiente = self.primero
+            self.primero.anterior = aux
+            self.primero = aux
+        self.unir_nodos()
+    
+    def agregar_final(self,dato):
+        if self.vacia():
+            self.primero = self.ultimo = Nodo(dato)
+        else:
+            aux = self.ultimo
+            self.ultimo = aux.siguiente = Nodo(dato)
+            self.ultimo.anterior = aux
+        self.unir_nodos()
+    
+    def unir_nodos(self):
+        self.primero.anterior = self.ultimo
+        self.ultimo.siguiente = self.primero
+
+    def RecorrerInicioFin(self):
+        aux = self.primero
+        while aux:
+            print(aux.dato)
+            aux = aux.siguiente
+            if aux == self.primero:
+                break
+    
+    def RecorrerFinInicio(self):
+        aux = self.ultimo
+        while aux:
+            print(aux.dato)
+            aux = aux.anterior
+            if aux == self.ultimo:
+                break
+
+from os import system
+system("cls")
+
+s = ListaCircularDoble()
+
+s.agregar_final(12)
+s.agregar_final(45)
+s.agregar_final(18)
+s.agregar_final(36)
+s.agregar_final(10)
+
+s.RecorrerInicioFin()
+print("*"*25)
+s.RecorrerFinInicio()
+    
